276a0e07a40b12210b39ddf04f03f42a52598d9c
Freddy Munive <munivef@yahoo.com>
2020-04-30 16:18:29 -0500

    adding testing and class setup

---
+++ leaderboard.rb
@@ -0,0 +1,29 @@
+require 'terminal-table'
+require 'http'
+
+
+class GitHub
+
+  URL_ROOT_API = "https://api.github.com"
+  # URL_TO_TEST = https://github.com/codeableorg/week-2-extended-project
+  # doc = https://developer.github.com/v3/
+
+  def get_collaborators
+  end
+
+  def see_PRs_reviwed_count
+  end
+
+  def see_comment_count
+  end
+
+  def see_ratio
+  end
+
+  def sort_by_PRs
+  end
+
+end
+
+codeable_test = GitHub.new
+
